cf6124bfff1a9220cdfaecb2f5b1b71deab84e57
Nityaanand12 <38303135+Nityaanand12@users.noreply.github.com>
2022-07-05 23:06:07 +0530

    Update Stackfile.py

--- Stackfile.py
+++ Stackfile.py
@@ -11,24 +11,24 @@ class Stack:
     def __init__(self, value):
         new_node = Node(value)
         self.top = new_node
-        self.length = 1
+        self.height = 1
         
     def push(self, value):
         new_node = Node(value)
-        if self.length == 0:
+        if self.height == 0:
             self.top = new_node
         else:
             new_node.next = self.top
             self.top = new_node
-        self.length += 1
+        self.height += 1
     
     def pop(self):
-        if self.length == 0:
+        if self.height == 0:
             return 
         else:
             temp = self.top.next
             self.top = temp
-            self.length -= 1
+            self.height -= 1
     
     def print_list(self):
         temp = self.top
@@ -42,5 +42,6 @@ stack.push(5)
 stack.push(6)
 stack.pop()
 stack.print_list()
+print(stack.height)
 
 
